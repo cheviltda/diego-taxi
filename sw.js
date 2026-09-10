dfe0ec8746359ae9fbcf7a186db25dee079ce20d
cheviltda <chevi.ltda@gmail.com>
2026-09-09 22:24:25 -0300

    Add files via upload
    Diego taxi

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const CACHE='diego-taxi-v2';
-const ASSETS=['./','./index.html','./manifest.json','./sw.js'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+const CACHE='diego-taxi-v41';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
